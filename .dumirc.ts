3e9563a3105166cdc3ccbd14d600c3caa1db5cb1
cassie <cassie.6.wang@gmail.com>
2026-02-10 15:05:08 +0800

    chore: update dui docs

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,14 +2,15 @@ import { defineConfig } from 'dumi';
 
 const isDev = process.env.NODE_ENV === 'development';
 
-const publicPath = isDev ? '/' : '/laf-store/';
+const publicPath = isDev ? '/' : '/cassie-store/';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+
   base: publicPath,
-  publicPath: publicPath,
+  publicPath: publicPath, // 静态资源的公共路径
   resolve: {
-    atomDirs: [{ type: 'guide', dir: 'src' }]
+    atomDirs: [{ type: 'guide', dir: 'src' }],
   },
   locales: [
     { id: 'zh-CN', name: '中文' },
@@ -20,8 +21,14 @@ export default defineConfig({
     logo: publicPath + 'logo-fill_100.png',
     name: 'React Model Store',
     nav: {
-      'zh-CN': [{ title: '指南', link: '/guides' }, { title: 'API 文档', link: '/api' }],
-      'en-US': [{ title: 'Guide', link: '/en-US/guides' }, { title: 'API Doc', link: '/en-US/api' }],
+      'zh-CN': [
+        { title: '指南', link: '/guides' },
+        { title: 'API 文档', link: '/api' },
+      ],
+      'en-US': [
+        { title: 'Guide', link: '/en-US/guides' },
+        { title: 'API Doc', link: '/en-US/api' },
+      ],
     },
     showLineNum: true,
   },
